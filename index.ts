f6e34119d401b2a8029f53976a4fe1a8c5bcb4d8
Minh Dat <ngominhdat2004@gmail.com>
2026-06-10 23:13:04 +0700

    1. Settup GraphQL

---
+++ index.ts
@@ -0,0 +1,44 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import * as database from "./config/database";
+
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@as-integrations/express5";
+
+dotenv.config();
+database.connect();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+const startServer = async () => {
+  const typeDefs = `
+    type Query {
+      hello: String
+    }
+  `;
+
+  const resolvers = {
+    Query: {
+      hello: () => "Hello World!",
+    },
+  };
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  await server.start();
+
+  app.use(express.json());
+
+  app.use("/graphql", expressMiddleware(server));
+
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+    console.log(`GraphQL endpoint: http://localhost:${port}/graphql`);
+  });
+};
+
+startServer();
---
+++ models/article.model.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+export const articleSchema = new mongoose.Schema({
+    title: String,
+    avatar: String,
+    description: String,
+    deleted: {
+        type: Boolean,
+        default: false
+    },
+    deletedAt: Date
+},
+    {timestamps: true}
+);
+
+export const Article = mongoose.model('Article', articleSchema, 'articles');
+
+export default Article;
